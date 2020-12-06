aaba2b75c04bf232a132cf75808f0115353c1f72
brown9804 <belindabrownr04@gmail.com>
2020-12-06 00:23:10 -0600

    Part C - Layour and Timing - to be tested

---
+++ Design_Complete_Process/layout/README.md
@@ -0,0 +1,97 @@
+# Qflow Analysis
+
+There is two folders one for the counter_b4 and another one for the counter_b32. If follows the ideal setup based on the reference of http://opencircuitdesign.com/qflow/.
+
+![image](https://github.com/brown9804/Designs_Verification/blob/main/Qflow_Analysis/docs/img/ideal_setup.png)
+
+
+Aspects analyzed:
+
+*. Frequency.
+
+*. Area.
+
+*. Number and type of gates.
+
+*. Path delays.
+
+*. Place and route.
+
+## Dependencies
+
+Make sure qflow is install:
+
+~~~~
+$ sudo apt-get update
+$ sudo apt-get install qflow
+~~~~
+
+* yosys:
+
+Verilog parser/synthesis (yosys)
+* graywolf:
+
+Cell and pin placement
+
+* qrouter 1.3:
+
+Detail router
+
+* magic 8.1:
+
+Layout viewer
+
+
+
+## Instructions 
+
+~~~~
+<inside specific project> make qflow
+<inside specific project> make stat
+<inside specific project> magic
+~~~~
+
+### Magic for counter_b4 
+~~~~~~
+<inside magic terminal> lef read /usr/share/qflow/tech/osu035/osu035_stdcells.lef
+<inside magic terminal> def read ././layout/counter_b4.def
+<inside magic terminal> grid 1.6um 2.0um
+<inside magic terminal> measure horizontal
+<inside magic terminal> measure vertical
+~~~~~~
+
+### Magic for counter_b32
+~~~~~~
+<inside magic terminal> lef read /usr/share/qflow/tech/osu035/osu035_stdcells.lef
+<inside magic terminal> def read ././layout/counter_b32.def
+<inside magic terminal> grid 1.6um 2.0um
+<inside magic terminal> measure horizontal
+<inside magic terminal> measure vertical
+~~~~~~
+
+### Tech change 
+~~~~~~~
+<inside specific project> make qflow
+~~~~~~~
+
+Now inside `qflow_vars.sh` set:
+
+~~~~
+<set> techdir=/usr/share/qflow/tech/osu050
+<set> techname=osu050
+~~~~
+
+Run the commands inside ` qflow_exec.sh ` one by one:
+
+~~~~~
+/usr/lib/qflow/scripts/synthesize.sh /home/belindabrown/Desktop/Designs_Verification//Design_Complete_Process/layout/counter_b4 counter_b4 
+/usr/lib/qflow/scripts/placement.sh -d /home/belindabrown/Desktop/Designs_Verification//Design_Complete_Process/layout/counter_b4 counter_b4 
+# /usr/lib/qflow/scripts/vesta.sh /home/belindabrown/Desktop/Designs_Verification//Design_Complete_Process/layout/counter_b4 counter_b4 
+/usr/lib/qflow/scripts/router.sh /home/belindabrown/Desktop/Designs_Verification//Design_Complete_Process/layout/counter_b4 counter_b4 
+# /usr/lib/qflow/scripts/placement.sh -f -d /home/belindabrown/Desktop/Designs_Verification//Design_Complete_Process/layout/counter_b4 counter_b4 
+# /usr/lib/qflow/scripts/router.sh /home/belindabrown/Desktop/Designs_Verification//Design_Complete_Process/layout/counter_b4 counter_b4 
+# /usr/lib/qflow/scripts/cleanup.sh /home/belindabrown/Desktop/Designs_Verification//Design_Complete_Process/layout/counter_b4 counter_b4 
+# /usr/lib/qflow/scripts/display.sh /home/belindabrown/Desktop/Designs_Verification//Design_Complete_Process/layout/counter_b4 counter_b4 
+~~~~~
+
+And magic using `osu050` instead of `osu035`.
---
+++ Design_Complete_Process/layout/projects/counter_b4/source/counter_b4.v
@@ -0,0 +1,176 @@
+// ##--------------------------------COUNTER_B4------------------------------------
+// ##
+// ##        Copyright (C) 2020 by
+// ##        Belinda Brown Ramírez
+// ##        timna.brown@ucr.ac.cr
+// ##
+// ##-----------------------------------------------------------------------------
+
+// Counter general design
+// considering:
+// Count up.
+// Count down.
+// Count three at a time down.
+// Parallel charging.
+
+`ifndef COUNTER_B4
+`define COUNTER_B4
+
+
+module counter_b4(
+input wire b4_enable,
+input wire b4_clk,
+input wire b4_reset,
+input wire [1:0] b4_mode, // choose from 00, 01, 10, 11
+input wire [3:0] b4_D,
+output reg b4_load,
+output reg b4_rco, //  2^nbits - 1 = #
+output reg [3:0] b4_Q
+);
+
+
+reg [3:0] mem;
+
+always @(posedge b4_clk) begin
+    if (b4_reset == 1) begin
+      b4_load <= 0; // label b4_load b4_mode ON/OFF
+      b4_rco <= 0; // Ripple-Carry Out
+      b4_Q <= 4'b0000;
+    end // end on -> reset == 1 clean data
+
+  else begin // reset == 0
+
+    if (b4_enable == 0) begin
+      b4_Q <= 4'b0000;
+      b4_load <= 0;
+      b4_rco <= 0;
+    end // end if b4_enable == 0 & b4_reset == 0 -> Q = 00
+    else begin // if enable == 1
+      // MODO = 00 − > Q + 3
+      // MODO = 01 − > Q - 1
+      // MODO = 10 − > Q + 1
+      // MODO = 11 − > D
+////////////////
+////////// CASES 
+////////////////
+
+   case(b4_mode)
+
+    2'b00:   begin
+      mem <= mem + 1;
+      b4_load <= 0;
+      //  * //
+      if (mem == (2**4 - 1)) begin // next stage when is 15  FLAG because goes to 0
+        b4_rco <= 1;
+      end // b4_rco == 1
+
+      else begin // same stage
+        b4_rco <= 0;
+      end // b4_rco ==0
+    end // end mode 0 
+
+    //////////////////////////// * ////////////////////
+
+    2'b01: begin
+      mem <= mem - 1;
+      b4_load <= 0;
+        //  * //
+      if (mem == 4'b0000)begin // next stage when is 0 FLAG becase -1 F
+        b4_rco <= 1;
+      end // b4_rco == 1
+
+      else begin // same stage
+        b4_rco <= 0;
+      end // b4_rco ==0
+    end // end mode 01
+
+
+    //////////////////////////// * ////////////////////
+
+    2'b10: begin
+      mem <= mem - 3;
+      b4_load <= 0;
+      //  * //
+      if (mem <= 2)begin // 
+        b4_rco <= 1;
+      end // b4_rco == 1
+
+      else begin // same stage
+        b4_rco <= 0;
+      end // b4_rco ==0
+    end // end mode 10 
+
+    //////////////////////////// * ////////////////////
+
+    2'b11: begin 
+      mem <= b4_D;
+      b4_load <= 1; // charging
+      ///////////////////////
+    // //   MODE 11
+    // //   RCO HALF
+    // //  UnComment this part 
+    //   //////////////////////////
+      b4_rco <= 0; 
+    end
+    //////////////////////////// * ////////////////////
+    default: begin // b4_mode != 00,01,10,11
+      mem <= mem;
+    end
+
+   endcase // end cases 
+
+    end // end if b4_enable on
+  end // end else zz
+end // end b4_clk
+
+always @(*) begin
+ b4_Q = mem;
+end
+
+
+// // /////////////////////
+// // // MODE 
+// // /// RCO HALF
+// //// Uncomment to activate
+// //////////////////////
+always @(negedge b4_clk) begin
+   case(b4_mode)
+
+   2'b00: begin 
+     if (b4_rco == 1) begin 
+      b4_rco <= 0;
+     end 
+   end // end charge mode
+
+   2'b01: begin 
+     if (b4_rco == 1) begin 
+      b4_rco <= 0;
+     end 
+   end // end charge mode
+
+   2'b10: begin 
+     if (b4_rco == 1) begin 
+      b4_rco <= 0;
+     end 
+   end // end charge mode
+
+
+   2'b11: begin 
+      b4_rco <= 0;
+     end   
+
+  default: begin
+    b4_rco <= b4_rco;
+  end
+endcase
+end 
+
+endmodule
+
+
+// Local Variables:
+// verilog-library-directories:("."):
+// verilog-auto-wire-type:\"logic\"
+// End:
+
+`endif
